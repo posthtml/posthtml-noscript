8721151d2704a49c66208c9550b48d90293c9d0a
Eric Liu <ericyl.us@gmail.com>
2019-08-16 18:19:41 -0700

    test: update unit snapshot

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -4,8 +4,14 @@ import { noscript } from '../';
 describe('posthtmlNoscript', () => {
   test('noscript matches snapshot', () => {
     posthtml()
-      .use(noscript())
-      .process(`<body></body>`)
+      .use(
+        noscript({ content: 'You need to enable JavaScript to run this app.' })
+      )
+      .process(
+        `<body>
+          <div id="root"></div>
+        </body>`
+      )
       .then((result: { html: string }) => {
         expect(result.html).toMatchSnapshot();
       });
