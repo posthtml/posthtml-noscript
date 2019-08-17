77582bf8f79eff95b9403fef0713edd183ce4099
Eric Liu <ericyl.us@gmail.com>
2019-08-16 18:19:10 -0700

    feat: insert noscript in body element

--- src/noscript.ts
+++ src/noscript.ts
@@ -1,9 +1,24 @@
-import { ITree } from 'posthtml';
+import { INode, ITree } from 'posthtml';
+import parser from 'posthtml-parser';
 
-function noscript() {
+function noscript(options: IOptions) {
   return function plugin(tree: ITree) {
-    return tree;
+    tree.match({ tag: 'body' }, node => {
+      let content: INode[] = [];
+
+      if (node.content) {
+        content = [...node.content];
+      }
+
+      content.push({ tag: 'noscript', content: parser(options.content) });
+
+      return { ...node, content };
+    });
   };
+}
+
+interface IOptions {
+  content: string;
 }
 
 export { noscript };
